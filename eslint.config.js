020c489169cd67350977e76beb469b6aad84780e
SeanJaeger1 <seanljaeger@gmail.com>
2025-05-09 10:11:00 +0700

    Upgraded linter

--- eslint.config.js
+++ eslint.config.js
@@ -1,30 +1,44 @@
-module.exports = {
-  env: {
-    es2021: true,
-    node: true,
+import eslint from "@eslint/js"
+import prettierPlugin from "eslint-plugin-prettier"
+import prettier from "eslint-config-prettier"
+
+export default [
+  eslint.configs.recommended,
+  {
+    ignores: ["node_modules/", "dist/", "build/"],
   },
-  extends: ["eslint:recommended", "plugin:prettier/recommended"],
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  rules: {
-    "no-console": ["error", { allow: ["error", "warn", "info"] }],
-    semi: ["error", "never"],
-    quotes: ["error", "double"],
-    "object-curly-spacing": ["error", "always"],
-    "comma-dangle": ["error", "only-multiline"],
-    "no-unused-vars": ["warn"],
-    "arrow-parens": ["error", "always"],
-    "max-len": [
-      "error",
-      {
-        code: 100,
-        ignoreComments: true,
-        ignoreStrings: true,
-        ignoreTemplateLiterals: true,
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        // This replaces the old "env" property
+        es2021: true,
+        node: true,
       },
-    ],
+    },
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      "no-console": ["error", { allow: ["error", "warn", "info"] }],
+      semi: ["error", "never"],
+      quotes: ["error", "double"],
+      "object-curly-spacing": ["error", "always"],
+      "comma-dangle": ["error", "only-multiline"],
+      "no-unused-vars": ["warn"],
+      "arrow-parens": ["error", "always"],
+      "max-len": [
+        "error",
+        {
+          code: 100,
+          ignoreComments: true,
+          ignoreStrings: true,
+          ignoreTemplateLiterals: true,
+        },
+      ],
+      "prettier/prettier": "error",
+    },
   },
-  ignorePatterns: ["node_modules/", "dist/", "build/"],
-}
+  prettier,
+]
